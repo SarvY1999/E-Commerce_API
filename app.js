b0c81147c8078a4ea8e9545de054b863f5978830
SarvY1999 <sarveshy193@gmail.com>
2023-07-01 20:17:55 +0530

    adds app.js and db connet

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('E-Commerce API');
+const express = require('express');
+require('express-async-errors');
+const app = express();
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('E-Com Api')
+})
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`listening on ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
