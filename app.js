5f1826d9baade431f8210fd56921905f2caceb40
SarvY1999 <sarveshy193@gmail.com>
2023-07-06 21:27:38 +0530

    adds cookies(signedcookies)

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-require('express-async-errors');
-const app = express();
 require('dotenv').config();
+require('express-async-errors');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./db/connect');
 const notFound = require('./middleware/not-found');
 const errorHandler = require('./middleware/error-handler');
 const authRouter = require('./routes/authRouter');
 
+const app = express();
+
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
+    console.log(req.signedCookies);
     res.send('E-Com Api')
 })
 app.use('/api/v1/auth', authRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const { createJwt, isTokenValid, attachCookiesToResponse } = require('../utils/jwt');
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -9,8 +10,10 @@ const register = async (req, res) => {
     if (ifExist) {
         throw new CustomError.BadRequestError('Email Already Exist');
     }
-    const user = await User.create(name, email, password);
-    res.status(StatusCodes.CREATED).json(user)
+    const user = await User.create({ name, email, password });
+    const tokenUser = { name: user.name, userId: user._id, role: user.role }
+    attachCookiesToResponse({ res, user: tokenUser });
+    res.status(StatusCodes.CREATED).json({ user: tokenUser })
 };
 
 const login = async (req, res) => {
